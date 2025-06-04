9af704d2bfdbb42172822d1f423488ee771e6eec
Maya Do <meiyeedo@gmail.com>
2025-06-04 18:37:40 +0100

    Set up character classes + camera components

---
+++ Game/Source/Space_Witch/Space_Witch.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Space_Witch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Space_Witch, "Space_Witch" );
